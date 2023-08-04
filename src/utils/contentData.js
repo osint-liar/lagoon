5b14a19c483f4a47c080dfbd88301f9d8d3dd698
yooper <dcardin2007@gmail.com>
2023-08-04 09:29:42 -0400

    update copy

--- src/utils/contentData.js
+++ src/utils/contentData.js
@@ -1,27 +1,21 @@
 const contentData = [
   {
-    title: "Configure other identity providers",
-    link: "https://auth0.com/docs/connections",
+    title: "Contribute to this Open Source Project",
+    link: "https://github.com/osint-liar/lagoon",
     description:
-      "Auth0 supports social providers as Facebook, Twitter, Instagram and 100+, Enterprise providers as Microsoft Office 365, Google Apps, Azure, and more. You can also use any OAuth2 Authorization Server."
+      "Contribute your skills to grow the OSINT abilities of this lagoon."
   },
   {
-    title: "Enable Multifactor Authentication",
-    link: "https://auth0.com/docs/multifactor-authentication",
+    title: "Create your own Lagoon",
+    link: "https://github.com/osint-liar/lagoon",
     description:
-      "Add an extra layer of security by enabling Multi-factor Authentication, requiring your users to provide more than one piece of identifying information. Push notifications, authenticator apps, SMS, and DUO Security are supported."
+      "Fork the repo and build your own Lagoon with your own tooling."
   },
   {
-    title: "Anomaly Detection",
-    link: "https://auth0.com/docs/anomaly-detection",
-    description:
-      "Auth0 can detect anomalies and stop malicious attempts to access your application. Anomaly detection can alert you and your users of suspicious activity, as well as block further login attempts."
-  },
-  {
-    title: "Learn About Rules",
-    link: "https://auth0.com/docs/rules",
-    description:
-      "Rules are JavaScript functions that execute when a user authenticates to your application. They run once the authentication process is complete, and you can use them to customize and extend Auth0's capabilities."
+    title: "Recommend a tool to integrate",
+    link: "https://github.com/osint-liar/lagoon/issues",
+    description: "Recommend a browser based tool to integrate or link into this Lagoon"
+
   }
 ];
 
